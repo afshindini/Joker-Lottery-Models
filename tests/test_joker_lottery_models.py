51e0c9ce3b93477f7822e4db3316956fe9461383
Afshin Dini <afshin_dini@yahoo.com>
2025-02-07 00:17:51 +0200

    bump version

--- tests/test_joker_lottery_models.py
+++ tests/test_joker_lottery_models.py
@@ -5,4 +5,4 @@ from joker_lottery_models import __version__
 
 def test_version() -> None:
     """Unit test for checking the version of the code"""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.2.0"
